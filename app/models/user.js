0df4a4f6e5c37ce68454718dbc6a34005d730159
Philip <Philip.zdb@gmail.com>
2018-01-24 11:15:25 +0100

    Adds user model with attributes

---
+++ app/models/user.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  profilePicture: DS.attr('string'),
+  age: DS.attr('number')
+  description: DS.attr('string')
+});
