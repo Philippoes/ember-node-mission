b3986f20d9cd89031f03af987efa0f34a1c4cfa1
Philip <Philip.zdb@gmail.com>
2018-01-26 11:01:46 +0100

    Removes route action and puts delete user in controller action

--- app/pods/users/route.js
+++ app/pods/users/route.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    deleteUser(user) {
-      user.destroyRecord().then(() => {
-        this.transitionTo('users')
-      })
-    }
-  }
 });
